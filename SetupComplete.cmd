9e0e83bd2ff3a6153975a190792d4d02a8a85bc7
Goran Štambuk <98338194+GorstakZD@users.noreply.github.com>
2022-06-25 21:07:46 +0200

    Add files via upload

---
+++ SetupComplete.cmd
@@ -0,0 +1,13 @@
+@echo off
+bcdedit /set disabledynamictick yes
+bcdedit /set useplatformtick yes
+bcdedit /timeout 3
+bcdedit /set nx OptIn
+bcdedit /set bootux disabled
+bcdedit /set bootmenupolicy legacy
+bcdedit /set tscsyncpolicy Enhanced
+bcdedit /set quietboot yes
+bcdedit /set {globalsettings} custom:16000067 true
+bcdedit /set {globalsettings} custom:16000069 true
+bcdedit /set {globalsettings} custom:16000068 true
+del /q /f "%0"
